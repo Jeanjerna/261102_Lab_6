55756046c72607c22096f3a742d438420b78983e
Chaichan <chaichanchaisak2549@gmail.com>
2024-11-29 15:48:13 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,31 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    int num , even = 0 , odd = 0;
+    
+    while (true){
+        
+        cout << "Enter an integer: ";
+        cin >> num;
+        if ((num % 2) == 0){
+            if(num == 0){
+                
+                break;
+            }else{
+                
+                even++;
+                
+            }
+        }else{
+            
+            odd++;
+            
+        }
+        
+    }
+    
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd << "\n";
     return 0;
 }
